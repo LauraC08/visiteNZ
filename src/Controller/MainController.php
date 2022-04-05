466ea234118cf8267552b4a0e082c823857ef039
LauraC08 <laura-chenais@hotmail.fr>
2022-04-05 17:20:33 +0200

    translations

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,12 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(TranslatorInterface $translator): Response
     {
-        return $this->render('main/index.html.twig');
+        $translated = $translator->trans('Symfony is great',
+    [],
+    'messages',
+    'fr_FR');
+        return $this->render('main/index.html.twig',
+        ['translated'=> $translated]);
     }
 }
