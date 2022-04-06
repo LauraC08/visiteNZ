84bb5ed166fe626a095ab7566317ed911b9bab05
MaelleDaniel <maelledaniel2@gmail.com>
2022-04-06 11:50:32 +0200

    fixed_bug_addRoute

--- src/Controller/ExperienceController.php
+++ src/Controller/ExperienceController.php
@@ -21,7 +21,7 @@ class ExperienceController extends AbstractController
         ['experiences'=>$exRep->findAll()]);
     }
 
-    #[Route('/{id}', name: 'app_experience_details', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_experience_details', requirements:['id'=>'\d+'], methods: ['GET'])]
     public function details($id, ExperienceRepository $repo): Response
     {
         $experience=$repo->find($id);
@@ -31,7 +31,7 @@ class ExperienceController extends AbstractController
         return $this->render('experience/details.html.twig', ['experience'=>$experience]);
     }
 
-    #[Route('/add', name: 'app_experience_add', methods: ['GET', 'POST'])]
+    #[Route('/add', name: 'app_experience_new', methods: ['GET','POST'])]
     public function add(Request $request, EntityManagerInterface $em): Response
     {
         $experience= new Experience();
@@ -47,20 +47,20 @@ class ExperienceController extends AbstractController
         return $this->render('experience/add.html.twig', compact('viewForm'));
     }
 
-    #[Route('/{id}/edit', name: 'app_experience_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_experience_edit', requirements:['id'=>'\d+'], methods: ['GET', 'POST'])]
     public function edit(Request $request, Experience $experience, ExperienceRepository $exRep): Response
     {
-        $form= $this->createForm(ExperienceType::class, $experience);
-        $form->handleRequest($request);
-        if($form->isSubmitted( ) && $form->isValid()){
+        $viewForm= $this->createForm(ExperienceType::class, $experience);
+        $viewForm->handleRequest($request);
+        if($viewForm->isSubmitted( ) && $viewForm->isValid()){
             $exRep->add($experience);
             return $this->redirectToRoute('app_experience',[], Response::HTTP_SEE_OTHER);
         }
-        return $this->renderForm('experience/edit.html.twig', compact('experience', 'form'));
+        return $this->renderForm('experience/edit.html.twig', compact('experience', 'viewForm'));
     }
 
 
-    #[Route('/{id}/delete', name: 'app_experience_delete', methods: ['POST'])]
+    #[Route('/{id}/delete', name: 'app_experience_delete', requirements:['id'=>'\d+'], methods: ['POST'])]
     public function delete(Experience $experience, ExperienceRepository $exRep): Response
     {
         //PENSER à RAJOUTER SEASURF
